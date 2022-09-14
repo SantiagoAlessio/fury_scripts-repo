2407f6c859d3d0114cf74a6ed4bb3b2135b39da3
Santiago Alessio <santiago.alessio@mercadolibre.com>
2022-09-14 15:40:56 -0300

    1.0rc script with card and add csv data

--- test-scripts-bazooka/vinculation-flow/functions.js
+++ test-scripts-bazooka/vinculation-flow/functions.js
@@ -3,9 +3,21 @@ function obtainCode(requestParams, response, context, ee, next){
 
     let code = uri.split("code=")[1].split("&")[0]
     context.vars.validationCode = code
+    if(response.statusCode === 500){
+        console.log("error ", {url: response.url, body: JSON.parse(response.body)})
+    }
+    next()
+}
+
+function debugResponse(requestParams, response, context, ee, next){
+    if(response.statusCode === 500){
+        console.log("error ", {url: response.url, body: JSON.parse(response.body)})
+    }
+
     next()
 }
 
 module.exports = {
-    obtainCode: obtainCode
+    obtainCode: obtainCode,
+    debugResponse: debugResponse
 }
